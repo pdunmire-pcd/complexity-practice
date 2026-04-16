76f1cc868a71f212810a8bcef50226808c155be7
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-04-15 19:58:48 -0700

    describeProduct time complexity answered

--- src/Main.java
+++ src/Main.java
@@ -51,7 +51,7 @@ public class Main {
   }
 
   // The time complexity is:
-  // YOUR ANSWER HERE
+  // O(n)
   public static void describeProduct(int[] nums) {
     System.out.println("About to compute the product of the array...");
     int product = computeProduct(nums);
